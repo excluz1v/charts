274977c18285a22bdccb72544345719d779f7b41
@Dmitriy-G <@dmitry-g@auto.kz>
2023-04-09 13:54:01 +0600

    dev: add authentication

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,12 +3,12 @@ import {UserInfo} from "../UserInfo/UserInfo";
 import {Navigation} from "../Navigation";
 import styles from './Header.module.css'
 
-export const Header =()=>{
+export const Header = () =>{
     return <header className={styles.Header}>
         <div className={styles.Header__wrapper}>
             <Logo/>
             <Navigation/>
-            <UserInfo user={true} />
+            <UserInfo user={false} />
         </div>
 
     </header>
