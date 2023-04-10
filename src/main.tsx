de250aa40fef511c0025a3cfcd9c968d66b60270
@Dmitriy-G <@dmitry-g@auto.kz>
2023-04-10 09:00:32 +0600

    dev: fix redirect

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,13 @@ import {Auth0Provider} from "@auth0/auth0-react";
 
 const CLIENT_ID=import.meta.env.VITE_CLIENT_ID
 const DOMAIN=import.meta.env.VITE_DOMAIN
+const redirectUrl = import.meta.env.PROD ? 'https://briskly.tech': window.location.origin
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
       <Auth0Provider domain={DOMAIN}
       clientId={CLIENT_ID}
-    authorizationParams={{redirect_uri:window.location.origin }}
+    authorizationParams={{redirect_uri:redirectUrl }}
       >
       <RouterProvider router={router} />
       </Auth0Provider>
